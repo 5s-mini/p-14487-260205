42b48aad455db8b6dd1351c0c144cd4b378042db
5s-mini <dnclsehd123@naver.com>
2026-02-05 14:21:11 +0900

    test: 테스트 케이스 추가

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -44,4 +44,20 @@ public class RqTest {
         String rst = rq.getParam("keywordType");
         assertThat(rst).isEqualTo("title");
     }
+
+    @Test
+    @DisplayName(value = "명령: 목록?page=1일 때, rq.getParamAsInt(\"page\") -> 1")
+    void t6() {
+        Rq rq = new Rq("목록?page=1");
+        int rst = rq.getParamAsInt("page");
+        assertThat(rst).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?page=10일 때, rq.getParamAsInt(\"page\") -> 10")
+    void t7() {
+        Rq rq = new Rq("목록?page=10");
+        int rst = rq.getParamAsInt("page");
+        assertThat(rst).isEqualTo(10);
+    }
 }
